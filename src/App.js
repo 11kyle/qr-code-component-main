9fd9a0f32bff13f849564e3c367ddcbedd3058f5
11kyle <11kyle.johnson@gmail.com>
2022-08-11 13:31:57 -0500

    Added box-border to parent container.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import QrCodeCard from './components/qrCodeCard';
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen box-border">
       <QrCodeCard />
     </div>
   );
