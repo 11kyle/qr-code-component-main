c9d5f982c0d0e632d642a25e9feb2cf163fb893b
11kyle <11kyle.johnson@gmail.com>
2022-08-11 13:02:29 -0500

    Adjusted styles.

--- src/components/qrCodeCard.js
+++ src/components/qrCodeCard.js
@@ -2,10 +2,10 @@ import qrCode from '../assets/image-qr-code.png';
 
 export default function QrCodeCard() {
   return (
-    <div className="flex flex-col justify-center items-center text-center p-4 mx-1 rounded-2xl bg-hslWhite max-w-[375px]">
+    <div className="flex flex-col justify-center items-center text-center p-4 mx-1 rounded-2xl bg-hslWhite max-w-[300px]">
       <img className="rounded-2xl" src={qrCode} alt="qr code" />
-      <h1 className="text-hslDarkBlue font-bold text-2xl p-5">Improve your front-end skills by building projects</h1>
-      <p className="text-hslGrayishBlue px-8 pb-6">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      <h1 className="text-hslDarkBlue font-bold text-xl px-5 pt-4 pb-3 leading-tight">Improve your front-end skills by building projects</h1>
+      <p className="text-hslGrayishBlue px-1 pb-4 text-[15px] leading-tight">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
   );
 }
